bd1a225e9d7ce4038f22c1c85a7e362cc76f2c4e
dev-tiendat <tiendatdev12301@gmail.com>
2025-05-27 11:32:42 +0700

    feat(api): add pagination types and sort order enum

--- src/types/api.ts
+++ src/types/api.ts
@@ -15,6 +15,32 @@ export type APIGenericResponse<T> = {
   response?: APIGenericResponseData<T>;
 };
 
+export enum SortOrder {
+  ASC = 'asc',
+  DESC = 'desc',
+}
+
+export type PaginationParams = {
+  page?: number;
+  limit?: number;
+  query?: string;
+  field?: string;
+  order?: SortOrder;
+};
+
+export type PaginationMeta = {
+  totalItems: number;
+  itemCount: number;
+  itemPerPage: number;
+  totalPages: number;
+  currentPage: number;
+};
+
+export type Pagination<T> = {
+  items: T[];
+  meta: PaginationMeta;
+};
+
 export enum ContentType {
   // json
   JSON = 'application/json;charset=UTF-8',
