e2bd3761a66a9ef70c885314ceefd957089b4051
dev-tiendat <tiendatdev12301@gmail.com>
2025-05-27 11:43:08 +0700

    feat(navigation): add User screen to ProfileNavigator

--- src/navigation/ProfileNavigator.tsx
+++ src/navigation/ProfileNavigator.tsx
@@ -1,9 +1,15 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import ProfileScreen from 'profile/ProfileScreen';
+import UserScreen from 'profile/UserScreen';
+import { User } from 'types/user';
 
 export type ProfileStackParamList = {
   Profile: undefined;
+  User: {
+    data?: User;
+    onSubmitSuccess?: () => void;
+  };
 };
 
 const Stack = createNativeStackNavigator<ProfileStackParamList>();
@@ -14,6 +20,7 @@ const ProfileNavigator = () => {
       screenOptions={{ headerShown: false }}
       initialRouteName="Profile">
       <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="User" component={UserScreen} />
     </Stack.Navigator>
   );
 };
